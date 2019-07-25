400d053adfbe140a675b3e2a600cbeaa788fb868
asaad21-meet <asaad21@meet.mit.edu>
2019-07-25 06:09:56 -0400

    Stopping here

---
+++ lab_5.py
@@ -0,0 +1,13 @@
+one = [1,2,3,4]
+two = [7,6,5,4]
+three = ["y1","friends", "fun"]
+print (one + two)
+print (one[3])
+one.remove(4)
+print(one)
+one.append(4)
+print(one)
+one.pop(1)
+print(one)
+one.sort()
+print(one)
